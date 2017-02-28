6813a76ca19b643032350b8bdc122362cc96acf2
jia <957016122@qq.com>
2017-02-28 19:29:25 +0800

    123

---
+++ home/app/Http/Controllers/SetController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class SetController extends Controller
+{
+    //账号设置
+    public function index(){
+
+        return view("set/index");
+    }
+
+    //个人主页
+    public function myself()
+    {
+        $user = "18840825602";
+        $user = DB::table('user')->where(['tel'=>$user])->orwhere(['email'=>$user])->first();
+        $id = $user['u_id'];
+        //用  用户ID 查询  user_information 表
+        $user_info = DB::table("user_information")->where(['u_id'=>$id])->first();
+
+        return view("set/myself",[
+            "user_info"=>$user_info,
+
+        ]);
+    }
+}
